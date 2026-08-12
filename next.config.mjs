d48fd10c18fa56fa1e2aff8263a80717816c98e9
frirsta <frirstaalikarem@gmail.com>
2026-08-12 13:23:57 +0200

    Set up Next.js with Cloudflare

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+};
+
+export default nextConfig;
+
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
